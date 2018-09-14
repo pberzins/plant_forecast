27e45c8fc6d3b164726b078b878ed60cb7e62b71
Peter Berzins <peter.m.berzins@gmail.com>
2018-09-14 10:13:50 -0700

    Graphing tools so can be done outside of Jupyter notebook

--- src/graphing_tools.py
+++ src/graphing_tools.py
@@ -65,27 +65,31 @@ def plot_over_time(X_train, X_test, y_train, y_test):
             label=f'Random Forest r2={rf_score:.2f} mse={rf_mse:.2f}', color='C1')
     ax.plot(X_test.index, lin_pred,
             label=f'Linear Regression r2={lin_score:.2f} mse={lin_mse:.2f}', color='C2')
-    #ax.plot(X_test.index, ridge_pred, label=f'Ridge Regression r2={ridge_score:.2f} mse={ridge_mse:.2f}', color='red')
-    #ax.plot(X_test.index, lasso_pred, label=f'Lasso Regression Predicted NDVI r2:{lasso_score}', color='blue')
+    ax.plot(X_test.index, ridge_pred,
+            label=f'Ridge Regression r2={ridge_score:.2f} mse={ridge_mse:.2f}', color='red')
+    ax.plot(X_test.index, lasso_pred,
+            label=f'Lasso Regression r2:{lasso_score:.2f} mse={lasso_mse:.2f}', color='blue')
 
-    #ax2 = ax.twinx()
-    #ax2.plot(X_test.index, X_test['TMAX'], color='Red',linestyle=':')
-    #ax2.plot(X_test.index, X_test['TMIN'], color='teal',linestyle=':')
+    ax2 = ax.twinx()
+    ax2.plot(X_test.index, X_test['TMAX'], color='Red', linestyle=':')
+    ax2.plot(X_test.index, X_test['TMIN'], color='teal', linestyle=':')
 
-    #ax3= ax.twinx()
-    #ax3.bar(X_test.index, -X_test['PRCP'].astype(float), 3,label='precipitation',color='blue')
-    #ax3.bar(X_test.index, -X_test['SNOW'].astype(float), 3,label='snow',color='yellow')
-    # ax3.set_ylim(-200,0)
+    ax3 = ax.twinx()
+    ax3.bar(X_test.index, -X_test['PRCP'].astype(float),
+            3, label='precipitation', color='blue')
+    ax3.bar(X_test.index, -X_test['SNOW'].astype(float),
+            3, label='snow', color='yellow')
+    ax3.set_ylim(-200, 0)
 
     ax.set_title(
         'Linear Model, Gradient Boosted, Random Forest, and Ridge Regression, \n Trained 2000-2015, Tested 2015-2017')
     ax.set_ylabel('NDVI scaled 0.0001')
     ax.set_xlabel('Date')
     #ax.fill_between(X_test.index, y_test, 0)
-    # ax.set_ylim(1800,4800)
+    ax.set_ylim(1800, 4800)
     ax.legend(loc=0, prop={'size': 16})
-    # ax2.legend(loc=1)
-    # ax3.legend(loc=2)
+    ax2.legend(loc=1)
+    ax3.legend(loc=2)
 
     plt.show()
 
@@ -109,4 +113,3 @@ def graph_lag_mse(range_list, gbr_mse_list, rf_mse_list, ridge_mse_list, gbr_sco
     plt.show()
 
     return None
-    
