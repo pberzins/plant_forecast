f3227139a618bd70c7bb5b005c23085e96aea481
Peter Berzins <peter.m.berzins@gmail.com>
2018-11-17 22:44:50 -0800

    Load from .py file

--- src/bayesian_model.py
+++ src/bayesian_model.py
@@ -1,2 +1,21 @@
+from src.final_product import PlantForecast
+import src.graphing_tools as gtools
+
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+import os
+import time
+import gdal
+
+import matplotlib.pyplot as plt
+import matplotlib
+
 import sampyl as smp
 from sampyl import np
+
+
+if __name__ == '__main__':
+    pf = PlantForecast()
